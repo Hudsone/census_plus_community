a9913dbe4aa3efe72c1012c16813d15111f3ad9c
Hsiwei Chang <hudsone@gmail.com>
2025-01-22 00:08:44 +0800

    Extract the test code from the main file.

--- CensusPlus/libs/LibWho-2.0/LibWho-2.0.lua
+++ CensusPlus/libs/LibWho-2.0/LibWho-2.0.lua
@@ -243,9 +243,7 @@ function lib.UserInfo(defhandler, name, opts)
       valid = false,
       inqueue = false,
       callback = {},
-      data = {
-        Name = args.name
-      },
+      data = {Name = args.name},
       last = now
     }
   end
@@ -847,67 +845,6 @@ WorldFrame:HookScript('OnMouseDown', function(_, _) hardwareEventTriggered() end
 for target, _ in pairs(lib['embeds']) do
   if type(target) == 'table' then lib:Embed(target) end -- if
 end                                                     -- for
-
----
---- Tests
----
-
----@class Testcase
----@field name string The name of the test.
----@field func function The function to test.
-
----@class Tester
----@field tests Testcase[] The list of tests.
----@field results boolean[] The list of results of the tests.
-
----@type Tester
-local tester = {tests = {}, results = {}}
-
-function tester:PushTest(name, func)
-  tinsert(self.tests, {name = name, func = func})
-end
-
-function tester:StartTest() for i, test in pairs(self.tests) do test.func(i) end end
-
-function tester:ReportTestResult(idx, result)
-  self.results[idx] = result
-  if #self.results == #self.tests then self:FinalizeTest() end
-end
-
-function tester:FinalizeTest()
-  local sPassed = '\124c0000FF00PASSED\124r'
-  local sFailed = '\124c00FF0000FAILED\124r'
-  print('Test results:')
-  local passed = 0
-  for i, result in pairs(self.results) do
-    print(string.format('%s: %s', result and sPassed or sFailed,
-                        self.tests[i].name))
-    if result then passed = passed + 1 end
-  end
-  print(string.format('Total %d/%d tests passed.', passed, #self.tests))
-end
-
-local function unitTest_Tester_ShouldReportTestResults(idx)
-  tester:ReportTestResult(idx, true)
-end
-
-local function functionalTest_Who_ShouldReturnResults(test_idx)
-  print('Note: Please click to enable the hardware event.')
-  lib:Who('0-500', function(query, result)
-    assert(#result >= 1)
-    assert(query == '0-500')
-    tester:ReportTestResult(test_idx, true)
-  end)
-end
-
-SLASH_WHOLIB_TEST1 = '/wholib-test'
-SlashCmdList['WHOLIB_TEST'] = function(msg)
-  tester:PushTest('unitTest_Tester_ShouldReportTestResults',
-                  unitTest_Tester_ShouldReportTestResults)
-  tester:PushTest('functionalTest_Who_ShouldReturnResults',
-                  functionalTest_Who_ShouldReturnResults)
-  tester:StartTest()
-end
 
 ---
 --- Debug Information
---
+++ CensusPlus/libs/LibWho-2.0/LibWhoTest.lua
@@ -0,0 +1,84 @@
+--[[
+    @file LibWho-Test.lua
+    @brief Test cases for LibWho-2.0.
+
+    This file contains the tests that can be run to verify the functionality of WhoLib.
+]]
+
+---@class Testcase
+---@field name string The name of the test.
+---@field func function The function to test.
+
+---@class Tester
+---@field tests Testcase[] The list of tests.
+---@field results boolean[] The list of results of the tests.
+local tester = {tests = {}, results = {}}
+local lib = LibStub('LibWho-2.0')
+if IntellisenseTrick_ExposeGlobal then lib = LibWho end
+
+--
+-- Test cases
+--
+-- Test case should be defined as the following format:
+-- local function <TestName>(test_idx)
+--   -- Test code here
+--   tester:ReportTestResult(test_idx, <TestResult>)
+-- end
+--
+-- After the test has been defined, you need to push the test to the tester.
+-- Just check the `Slash command` section for more information.
+--
+
+local function unitTest_Tester_ShouldReportTestResults(test_idx)
+  tester:ReportTestResult(test_idx, true)
+end
+
+local function functionalTest_Who_ShouldReturnResults(test_idx)
+  print('Note: Please click to enable the hardware event.')
+  lib:Who('0-500', function(query, result)
+    assert(#result >= 1)
+    assert(query == '0-500')
+    tester:ReportTestResult(test_idx, true)
+  end)
+end
+
+--
+-- Slash command
+--
+
+SLASH_WHOLIB_TEST1 = '/wholib-test'
+SlashCmdList['WHOLIB_TEST'] = function(msg)
+  tester:PushTest('unitTest_Tester_ShouldReportTestResults',
+                  unitTest_Tester_ShouldReportTestResults)
+  tester:PushTest('functionalTest_Who_ShouldReturnResults',
+                  functionalTest_Who_ShouldReturnResults)
+  tester:StartTest()
+end
+
+--
+-- Tester functions
+--
+
+function tester:PushTest(name, func)
+  tinsert(self.tests, {name = name, func = func})
+end
+
+function tester:StartTest() for i, test in pairs(self.tests) do test.func(i) end end
+
+function tester:ReportTestResult(idx, result)
+  self.results[idx] = result
+  if #self.results == #self.tests then self:FinalizeTest() end
+end
+
+function tester:FinalizeTest()
+  local sPassed = '\124c0000FF00PASSED\124r'
+  local sFailed = '\124c00FF0000FAILED\124r'
+  print('Test results:')
+  local passed = 0
+  for i, result in pairs(self.results) do
+    print(string.format('%s: %s', result and sPassed or sFailed,
+                        self.tests[i].name))
+    if result then passed = passed + 1 end
+  end
+  print(string.format('Total %d/%d tests passed.', passed, #self.tests))
+end
