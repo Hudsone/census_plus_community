c3ed60600c597e71413209cba5bb567c5eb1b0d0
Hsiwei Chang <hudsone@gmail.com>
2025-02-18 21:34:15 +0800

    Advance the version to 8.1.10.

--- CensusPlus.lua
+++ CensusPlus.lua
@@ -62,12 +62,7 @@ CPp.TZWarningSent = false;                    -- not used?
 --
 ]]
 
---[[
-local CensusPlus_Version_Major = '8'; -- changing this number will force a saved data purge
-local CensusPlus_Version_Minor = '0'; -- changing this number will force a saved data purge
-local CensusPlus_Version_Maint = '5';
-]]
-local projectVersion = '8.1.9'
+local projectVersion = '8.1.10'
 local versionNumbers = strsplittable('.', projectVersion)
 local CensusPlus_Version_Major = versionNumbers[1]
 local CensusPlus_Version_Minor = versionNumbers[2]
